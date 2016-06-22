0a6cb8756c9eb0d1f67cfacda1f2f48218353129
ImayaDismas <imayadismas@gmail.com>
2016-06-22 05:20:43 +0300

    Splash screen and App intro complete

---
+++ app/src/main/java/com/dismas/imaya/dyna_kenya/SplashScreenAlways.java
@@ -0,0 +1,40 @@
+package com.dismas.imaya.dyna_kenya;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by imaya on 6/22/16.
+ */
+public class SplashScreenAlways extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreenAlways.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
